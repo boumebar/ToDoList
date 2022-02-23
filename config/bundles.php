bb65c098b3bc6b260fdfd69c08523e8810b24b9d
boumediene <boume13@hotmail.com>
2022-02-23 20:06:55 +0100

    upgratedto4-4

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];

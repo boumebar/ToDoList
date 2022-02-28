7deb4db8c5e28f604f41148735998534dc26d8e7
boumediene <boume13@hotmail.com>
2022-02-28 22:44:19 +0100

    update 5.4

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,7 +10,7 @@ class DefaultController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction($mailerDsn)
     {
         return $this->render('default/index.html.twig');
     }
